b513bd354a2d19108a09c98608c7adb1667d2e72
Lennart <loesche.lennart@gmail.com>
2021-03-15 03:37:35 +0100

    Botinfo
    Botinfo - Bot Statistiken

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -5,13 +5,13 @@ module.exports = {
 	description: 'Gibt dir einen Einladungslink um den Bot auf deinem Server hinzuzufügen.',
 	aliases: ['getbot'],
 	execute(message, args, client) {
-        var embed = new Discord.MessageEmbed()
-        .setTitle(`${client.user.username} • Invite`)
-        .setThumbnail(client.user.displayAvatarURL())
-        .setDescription('Mit dem folgendem [Link](https://bit.ly/3op4lMm) kannst du den Bot auf deinen Server einladen.')
-        .setTimestamp(message.createdAt)
-        .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-        .setColor("#4680FC");
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`${client.user.username} • Invite`)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setDescription('Mit dem folgendem [Link](https://bit.ly/3op4lMm) kannst du den Bot auf deinen Server einladen.')
+            .setTimestamp(message.createdAt)
+            .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+            .setColor("#4680FC");
         message.channel.send(embed);
 	},
 };
