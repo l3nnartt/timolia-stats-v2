358c0b0a4581b7fd558bc40b55bd25aa5988a5d0
Lennart <loesche.lennart@gmail.com>
2021-03-17 13:51:00 +0100

    Serverinfo

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js', { token: 'Nzg4NDUwNzc0NjQ4NzUwMDkw.X9jsAQ.nyico7gV4EYpEJSSWt8QK2-6_ZI' });
-
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
