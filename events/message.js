6ddfc7f19cb84c19031fa57e3ab1ecde61662b63
l3nnartt <loesche.lennart@gmail.com>
2021-04-04 23:58:36 +0200

    New Logging System
    Richtlinienkonformes Loggingsystem, nur DMs werden geloggt, in File und auf dem Server als Embed

--- events/message.js
+++ events/message.js
@@ -1,12 +1,24 @@
 const fs = require('fs');
+const Discord = require("discord.js");
 
 module.exports = {
 	name: 'message',
-	execute(message) {
-		//Logging System
-		const timestamp = new Date();
-		fs.appendFile(`./debug.log`, `timestamp: ${timestamp};\tAuthor: ${message.author.tag};\tServer: ${message.guild};\tChannel: #${message.channel.name};\tContent: ${message.content};\n`, function (err) {
-			if (err) throw err;
-		});
+	execute(message, client) {
+		if (message.channel.type == 'dm') {
+			//TimeStamp
+			const timestamp = new Date()
+
+			//File Log
+			fs.appendFile(`./debug.log`, `timestamp: ${timestamp};\t Author: ${message.author.tag};\t Content: ${message.content};\n`, function (err) {
+				if (err) throw err;
+			});
+
+			//Discord Log
+			var embed = new Discord.MessageEmbed()
+				.setTitle(`${message.author.tag}`)
+				.addFields({ name: `Content:`, value: `${message.content}`})
+				.setFooter(`${timestamp}`)
+			client.channels.fetch('828385793528561694').then(channel => channel.send(embed));
+		}
 	},
 };
