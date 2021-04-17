70fd560a64ebe43bc615882e14beca2054f9c52a
l3nnartt <loesche.lennart@gmail.com>
2021-04-17 21:24:09 +0200

    Website
    Full Responsive Website

--- website/assets/footer.php
+++ website/assets/footer.php
@@ -1,4 +1,5 @@
-<p>
+<hr>
+<p class="container padding">
     karmatop.de ist eine Community Website und steht nicht in Verbindung mit der tixia UG (haftungsbeschränkt) oder dem Timolia Netzwerk.<br>
     Wir bemühen uns die Liste so aktuell wie möglich zu halten, trotzdem besteht keine Garantie auf Aktualität und Richtigkeit der Daten!
 <p>
--- website/index.php
+++ website/index.php
@@ -15,50 +15,53 @@
         <?php include"assets/navbar.php"; ?>
 
         <!--Cards-->
-        <div class="container-fluid padding">
+        <h1>karmatop.de</h1>
+
+        <!--Cards-->
+        <div class="container padding">
             <div class="row padding">
 
                 <!--Card 1-->
                 <div class="col-sm-4 mb-3">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-body">
                             <h4 class="card-title">Timolia Statistiken</h4>
                             <p>Auf dieser Website findest du die Karmatop Liste, welche alle Spieler die auf dem Timolia Achievment Community beinhaltet, und deren Karma auflistet. Wenn du mehr erfahren möchtest oder eine Frage hast, dann komm doch einfach auf den Timolia Achievment Community Discord</p>
-                            <a href="karmatop.php" class="btn btn-outline-primary">Zur Karmatop</a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="karmatop.php" class="btn btn-outline-primary w-100">Zur Karmatop</a>
                         </div>
                     </div>
                 </div>
 
                 <!--Card 2-->
                 <div class="col-sm-4 mb-3">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-body">
                             <h4 class="card-title">Timolia Achievement Community</h4>
                             <p>Neben unseren Online Diensten gibt es auch einen Discord Server auf welchem wir uns über Achievments unterhalten. Außerdem erhaltet ihr Informationen wenn es neue Achievments gibt, oder es andere Neuigkeiten gibt. Wenn du auch auf der Karmatop Liste eingetragen werden möchtest, oder fragen zu Achievments hast kannst du gerne auf dem Server vorbeischauen</p>
-                            <a href="https://discord.com/api/oauth2/authorize?client_id=803579214553022484&permissions=8&scope=bot" class="btn btn-outline-primary">Zum Discordbot</a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="https://discord.com/api/oauth2/authorize?client_id=803579214553022484&permissions=8&scope=bot" class="btn btn-outline-primary w-100">Zum Discordbot</a>
                         </div>
                     </div>
                 </div>
 
                 <!--Card 3-->
                 <div class="col-sm-4 mb-3">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-body">
                             <h4 class="card-title">Karmatop</h4>
                             <p>Auf dieser Website findest du die Karmatop Liste, welche alle Spieler die auf dem Timolia Achievment Community beinhaltet, und deren Karma auflistet. Wenn du mehr erfahren möchtest oder eine Frage hast, dann komm doch einfach auf den Timolia Achievment Community Discord</p>
-                            <a href="https://discord.gg/t9Jny6ds3E" class="btn btn-outline-primary">Discord Server beitreten</a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="https://discord.gg/t9Jny6ds3E" class="btn btn-outline-primary w-100">Discord Server beitreten</a>
                         </div>
                     </div>
                 </div>
 
             </div>
 
-        </div>
-
-        <!--Darkmode-->
-        <div class="custom-control custom-switch">
-            <input type="checkbox" class="custom-control-input" id="darkSwitch">
-            <label class="custom-control-label" for="darkSwitch">Dark Mode</label>
         </div>
 
         <!--Scripts-->
@@ -68,9 +71,6 @@
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-        <!--Darkmode-->
-        <script src="dark-mode-switch.js"></script>
 
     </body>
 
