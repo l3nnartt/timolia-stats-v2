351a6a76c2f1737cee57267276309fa0dde5bd80
l3nnartt <loesche.lennart@gmail.com>
2021-05-02 16:32:12 +0200

    Adds PixelSpace File

--- website/addon/pxl.php
+++ website/addon/pxl.php
@@ -1,8 +1,8 @@
 <?php
-if(!empty($_GET["name"]) && !empty($_GET["karma"]) && !empty($_GET["uuid"]) && strlen($_GET["name"]) >= 3 && strlen($_GET["name"]) <= 16 && strlen($_GET["uuid"]) == 36){
+if(!empty($_GET["name"]) && !empty($_GET["color"]) && !empty($_GET["uuid"]) && strlen($_GET["name"]) >= 3 && strlen($_GET["name"]) <= 16 && strlen($_GET["uuid"]) == 36){
     $name = $_GET["name"];
     $uuid = $_GET["uuid"];
-    $karma = $_GET["karma"];
+    $color = $_GET["color"];
     $serverid = "26c142208fc4cb3e6ed4ebc598d989b4848786ed";
     $response = file_get_contents("https://sessionserver.mojang.com/session/minecraft/hasJoined?username=".$name."&serverId=".$serverid);
     if(!empty($response)){
@@ -10,14 +10,21 @@ if(!empty($_GET["name"]) && !empty($_GET["karma"]) && !empty($_GET["uuid"]) && s
         $id = $json["id"];
         if($id == str_replace("-", "", $uuid))
             $mysqli = new mysqli('10.35.46.56:3306', 'k132321_bot', 'karmatopISTdie187gang', 'k132321_karmatop') or die(mysqli_error($mysqli));
-            $sqlselect = $mysqli->query("SELECT COUNT(*) FROM data WHERE uuid = '$uuid'");
+            $sqlselect = $mysqli->query("SELECT COUNT(*) FROM blocks WHERE uuid = '$uuid'");
+            $farbe = $mysqli->query("SELECT $color FROM blocks WHERE uuid = '$uuid'");
+            $total = $mysqli->query("SELECT total FROM blocks WHERE uuid = '$uuid'");
 
             if (intval($sqlselect->fetch_array()[0]) > 0) {
-                $mysqli->query("UPDATE data SET player='$name', karma='$karma' WHERE uuid='$uuid'") or die($mysqli->error);
+
+                $totalnew = (intval($total)) + 1;
+
+                $farbenew = (intval($farbe)) + 1;
+
+                $mysqli->query("UPDATE blocks SET total='$totalnew', $color='$farbenew' WHERE uuid='$uuid'") or die($mysqli->error);
             } else {
-                $mysqli->query("INSERT INTO data (player, karma, uuid) VALUES('$name', '$karma', '$uuid')") or die($mysqli->error);
+                $mysqli->query("INSERT INTO blocks (total, $color, uuid) VALUES('1', '1', '$uuid')") or die($mysqli->error);
             }
-            
+
             $mysqli->close();
 
             http_response_code(200);
