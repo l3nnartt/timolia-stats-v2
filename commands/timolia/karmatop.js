ca1004abd92a4316ae3cfcc88dabe717050ca280
l3nnartt <loesche.lennart@gmail.com>
2021-05-27 19:21:16 +0200

    Karmatop API
    PHP API für Karmatop integriert
    
    -> SQL Entfernt, keine Passwörter im Code

--- commands/timolia/karmatop.js
+++ commands/timolia/karmatop.js
@@ -1,39 +1,34 @@
 const Discord = require("discord.js");
-const mysql = require('mysql');
+const fetch = require('node-fetch');
 
 module.exports = {
 	name: 'karmatop',
 	cooldown: 5,
 	description: 'karmatop',
 	execute(message, args, client) {
+		const url = `https://hosting151773.a2e37.netcup.net/lennart/timolia/addon/karmatop.php`;
+		fetch(url, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json'
+		}})
+		.then((res) => res.json())
+		.then((data) => {
+			const fields = new Array();
+			data.forEach(function(item, index) {
+				fields.push({
+					name: `${index + 1}. ${item.player}`,
+					value: item.karma + ` Erfolgspunkte`
+				});
+			});
 
-		// SQL Connection
-		var con = mysql.createConnection({
-		host: "91.204.46.56",
-		user: "k132321_bot",
-		password: "karmatopISTdie187gang",
-		database: "k132321_karmatop"
+			var embed = new Discord.MessageEmbed()
+			.setTitle(`${client.user.username} • Karmatop`)
+			.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+			.setTimestamp(message.createdAt)
+			.setColor("#4680FC")
+			.addFields(fields);
+			message.channel.send(embed);
 		});
-
-		con.connect(function(err) {
-            con.query("SELECT * FROM data ORDER BY karma+0 DESC LIMIT 10", function (err, result) {
-                const fields = new Array();
-                result.forEach(function(item, index) {
-					fields.push({
-					  name: `${index + 1}. ${item.player}`,
-					  value: item.karma
-					});
-				  });
-                var embed = new Discord.MessageEmbed()
-				.setTitle(`${client.user.username} • Karmatop`)
-				.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-				.setTimestamp(message.createdAt)
-				.setColor("#4680FC")
-				.addFields(fields);
-                message.channel.send(embed);
-            });
-        });
     }
 };
-
-//Muss noch via API gelöst werden
