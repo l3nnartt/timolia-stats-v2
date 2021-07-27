4ae518568157da451372e6b8760d5ef486e759a8
Lennart <loesche.lennart@gmail.com>
2021-07-27 23:39:17 +0200

    Update Sever Invite Links

--- commands/timolia/server.js
+++ commands/timolia/server.js
@@ -9,7 +9,7 @@ module.exports = {
       .setTitle(`${client.user.username} • Community Server`)
       .setThumbnail(client.user.displayAvatarURL())
       .setDescription
-      (`[Timolia Achievements](https://discord.gg/t9Jny6ds3E)\n [JumpWorld Community](https://discord.gg/Y6DGketZYV)\n [Timolia Zeitung](https://discord.gg/ehC26pKWmZ)\n [Timolia Fails](https://discord.gg/cMJcCybQwR)\n [4rena Community](https://discord.gg/7SXq2EK8EP)\n [Splun Community](https://discord.gg/RGHcweAg2b)\n [Castles Community](https://discord.gg/dkDe6EKjNr)\n [SurvivalQuest Community](https://discord.gg/7EUxhJcuFh)\n`)
+      (`[Timolia Achievements](https://discord.gg/t9Jny6ds3E)\n [JumpWorld Community](https://discord.gg/Y6DGketZYV)\n [Timolia Zeitung](https://discord.gg/ehC26pKWmZ)\n [Timolia Fails](https://discord.gg/cMJcCybQwR)\n [4rena Community](https://discord.gg/7SXq2EK8EP)\n [Splun Community](https://discord.gg/RGHcweAg2b)\n [Castles Community](https://discord.gg/dkDe6EKjNr)\n [SurvivalQuest Community](https://discord.gg/7EUxhJcuFh)\n [Arcade Community](https://discord.gg/pcky7gvQsP)\n [InTime Community](https://discord.gg/4b2Gg8h9cv)`)
       .setTimestamp(message.createdAt)
       .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
       .setColor("#4680FC");
