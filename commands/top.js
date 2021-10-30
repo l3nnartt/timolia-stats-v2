d790633f0f7203114fcc9fb6fdf7dd822da3de06
l3nnartt <loesche.lennart@gmail.com>
2021-10-30 19:06:56 +0200

    geiler top command

--- commands/top.js
+++ commands/top.js
@@ -8,7 +8,7 @@ module.exports = {
         .setDescription('Gibt dir die Toplisten eines bestimmten Spielmodus')
         .addStringOption(option => option.setName('gamemode').setDescription('Wähle den Spielmodus von welchem du die Statistiken sehen möchtest').setRequired(true)),
     async execute(interaction, client) {
-        const gamemode = interaction.options.getString('gamemode');
+        const gamemode = interaction.options.getString('gamemode').toLowerCase();
 
         const options = {
             'method': 'GET',
@@ -22,20 +22,30 @@ module.exports = {
         
         request(options, function (error, response) {
             if (error) throw new Error(error);
-            console.log(response.body);
-            //const embed = new MessageEmbed()
-            //    .setTitle(`Top 10 • ${gamemode}`)
-            //    .setURL(`https://www.timolia.de/game/${gamemode}/leaderboard`)
-            //    .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-            //    .setTimestamp(interaction.createdAt)
-            //    .setColor("#4680FC");
-            //response.forEach(d => {
-            //    embed.addFields(
-            //        {
-            //            name: `${d.rank} ${d.name}`, value: `Punkte: ${d.points}`
-            //        }
-            //    );
-            //});
+            const raw = JSON.parse(response.body);
+            const embed = new MessageEmbed()
+                .setTitle(`Top 10 • ${gamemode}`)
+                .setURL(`https://www.timolia.de/game/${gamemode}/leaderboard`)
+                .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+                .setTimestamp(interaction.createdAt)
+                .setColor("#4680FC")
+
+            if (raw.data === null) {
+                const fail = new MessageEmbed()
+                    .setTitle(`Fehler`)
+                    .setDescription(`Es konnte keine Topliste für diesen Modus gefunden werden`)
+                    .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+                    .setTimestamp(interaction.createdAt)
+                    .setColor("#ff0000")
+                interaction.reply({embeds: [fail]});
+            } else {
+                raw.data.forEach(d => {
+                    embed.addFields({
+                        name: `${d.position}. ${d.name}`, value: `${d.points}`
+                    })
+                })
+                interaction.reply({embeds: [embed]});
+            }
         });
     },
 };
