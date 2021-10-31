cd299f97b45a9f75a0643b26d4a6e45f7232636d
l3nnartt <loesche.lennart@gmail.com>
2021-10-31 02:09:02 +0200

    Update Info Command + fix Sharding

---
+++ commands/info.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const config = require("../config.json");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Informationen über den Bot'),
+    async execute(interaction, client) {
+        const promises = [
+            client.shard.fetchClientValues('guilds.cache.size'),
+            client.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)),
+        ];
+
+        Promise.all(promises)
+        .then(results => {
+            const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+            const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+            const embed = new MessageEmbed()
+                .setTitle(`${client.user.username} • Informationen`)
+                .setThumbnail(client.user.displayAvatarURL())
+                .addFields(
+                    { name: 'Server', value: `${totalGuilds}`, inline: true },
+                    { name: 'Channel', value: `${client.channels.cache.size}`, inline: true },
+                    { name: 'Benutzer', value: `${totalMembers}`, inline: true },
+                    { name: 'Version', value: `${config.version}`, inline: true},
+                    { name: 'Entwickler', value: `<@398101340322136075>`, inline: true})
+                .setTimestamp(interaction.createdAt)
+                .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+                .setColor("#4680FC");
+            return interaction.reply({embeds: [embed]});
+        }).catch(console.error);
+    },
+};
