6f180b635a892d74b83c531362ead9ed44db6bed
l3nnartt <loesche.lennart@gmail.com>
2021-11-01 12:19:30 +0100

    fix

--- events/ready.js
+++ events/ready.js
@@ -66,38 +66,6 @@ module.exports = {
 				});
 		},15000);
 
-
-		//Register Commands
-		setInterval(() => {
-			const commands = [];
-			const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-			// Fill Array -> To clear Command List //comment
-			for (const file of commandFiles) {
-				const command = require(`./commands/${file}`);
-				commands.push(command.data.toJSON());
-			}
-
-			const rest = new REST({ version: '9' }).setToken(token);
-
-			(async () => {
-				try {
-					await rest.put(
-						// Guild Commands
-						//Routes.applicationGuildCommand(clientId, guildId),
-
-						// Global Commands
-						Routes.applicationCommands(clientId),
-						{ body: commands },
-					);
-					console.log(commands)
-					console.log('Successfully registered application commands.');
-				} catch (error) {
-					console.error(error);
-				}
-			})();
-		},3600000);
-
 		//Discord Message
 		const embed = new MessageEmbed()
 			.setDescription(`Bot erfolgreich gestartet`)
