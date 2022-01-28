34374bef1ef32afc82b4ad2e455238c9ce68a7a2
l3nnartt <loesche.lennart@gmail.com>
2022-01-28 16:25:37 +0100

    update win command

--- commands/win.js
+++ commands/win.js
@@ -7,18 +7,18 @@ module.exports = {
         .setName('win')
         .setDescription('Errechnet die Gewinnwahrscheinlichkeit in einem Modus von einem Spieler')
         .addStringOption(option => option.setName('player').setDescription('Wähle einen Spieler von welchem du die  Gewinnwahrscheinlichkeit sehen möchtest').setRequired(true))
-        .addStringOption(option => option.setName('spielmodus').setDescription('Wähle den Spielmodus von welchem du die  Gewinnwahrscheinlichkeit sehen möchtest').setRequired(true)),
+        .addStringOption(option => option.setName('gamemode').setDescription('Wähle den Spielmodus von welchem du die  Gewinnwahrscheinlichkeit sehen möchtest').setRequired(true)),
     async execute(interaction, client) {
         const player = interaction.options.getString('player');
-        const Gamemode = interaction.options.getString('spielmodus').toLowerCase();
-        const fixedGameMode = Gamemode === '1vs1' ? 'duels' : Gamemode;
+        const gamemode = interaction.options.getString('gamemode').toLowerCase();
+        const fixedGameMode = gamemode === '1vs1' ? 'duels' : gamemode;
         let playerhead = "https://cravatar.eu/helmavatar/" + player + "/60.png";
 
         mc.timolia(player)
         .then(result => {
             if (result.games === undefined || result.games[fixedGameMode] === undefined || result.errors === "User not found") {
                 const embed = new MessageEmbed()
-                    .setDescription(`Es konnte keine Gewinnwahrscheinlichkeit von **${player}** in **${Gamemode}** berechnet werden.`)
+                    .setDescription(`Es konnte keine Gewinnwahrscheinlichkeit von **${player}** in **${gamemode}** berechnet werden.`)
                     .setTimestamp(interaction.createdAt)
                     .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
                     .setColor("#ff0000");
@@ -30,7 +30,7 @@ module.exports = {
                 const embed = new MessageEmbed()
                     .setTitle(`${client.user.username} • Gewinnwahrscheinlichkeit`)
                     .setThumbnail(`${playerhead}`)
-                    .setDescription(`Der Spieler **${player}** hat in **${Gamemode}** eine Gewinnwahrscheinlichkeit von **${winProbability}%**`)
+                    .setDescription(`Der Spieler **${player}** hat in **${gamemode}** eine Gewinnwahrscheinlichkeit von **${winProbability}%**`)
                     .setTimestamp(interaction.createdAt)
                     .setFooter({ text: client.user.username, iconURL: client.user.displayAvatarURL() })
                     .setColor("#4680FC");
